76c6c5b2459e2c9d3f9c21559df647b34da890db
William Vásquez <75735138+Izmael19@users.noreply.github.com>
2021-06-21 22:22:08 -0600

    new

--- juegos.js
+++ juegos.js
@@ -28,42 +28,38 @@ let preguntas = [{
     }
 ]
 
-function shuffle(array) {
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
-    while (0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
-}
-
-shuffle(preguntas); //random array
 pregunta.innerHTML = preguntas[0].pregunta; //pregunta escrita
 let resp = preguntas[0].respuestas; //array de primera pregunta
-const opciones = resp.map(function(respuesta_c) {
+let opciones = resp.map(function(respuesta_c) {
     return respuesta_c
 });
 
+
 for (let i = 0; i < 4; i++) {
     respuesta[i].innerHTML = opciones[i];
-} //escribir preguntas 1
+} //escribir respuestas 1
+let y = 0;
 
 for (let j = 0; j < resp.length; j++) { //colorear
     respuesta[j].parentNode.addEventListener("click", function() {
+        y = y + 1;
         if (j == preguntas[j].correcta) {
             respuesta[j].parentNode.style.background = "green";
         } else {
             respuesta[j].parentNode.style.background = "red";
             respuesta[preguntas[0].correcta].parentNode.style.background = "green";
         };
-        /*
-                setTimeout(function() {
-                    pregunta.innerHTML = preguntas[1].pregunta;
-                }, 1000); */
+        setTimeout(function() {
+            respuesta[j].parentNode.style.background = "";
+            respuesta[preguntas[0].correcta].parentNode.style.background = "";
+            let resp = preguntas[y].respuestas; //array de siguiente pregunta
+            let opciones = resp.map(function(respuesta_c) {
+                return respuesta_c
+            });
+            for (let i = 0; i < 4; i++) {
+                respuesta[i].innerHTML = opciones[i];
+            }
+            pregunta.innerHTML = preguntas[y].pregunta;
+        }, 1000);
     })
 }
-console.log(preguntas);
